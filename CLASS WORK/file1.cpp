b1f4bd5407e7f8854693298e781f80f54ebc7fa4
Maaz Patwekar <242930825+nullpc@users.noreply.github.com>
2026-07-28 09:49:05 +0000

    commit

--- CLASS WORK/file1.cpp
+++ CLASS WORK/file1.cpp
@@ -1,12 +1,20 @@
 #include<iostream>
-namespace sycs4
-{
+ namespace sycs4
+ {
     int add()
     {
         int a,b,sum;
-        std :: cout<<"Enter Two Numbers : ";
-        std :: cin >>a>>b;
-        std :: cout << "Sum of Two Numbers is : "<< (a+b) << std :: endl;
+        std::cout<<"enter the two number";
+        std::cin>>a>>b;
+        std::cout<<"sum of two number is"<<(a+b);
         return 0;
     }
-}
+    int sub()
+    {
+        int a,b,sub;
+        std::cout<<"enter the two number";
+        std::cin>>a>>b;
+        std::cout<<"sub of two number is"<<(a-b);
+        return 0;
+    }
+ }
