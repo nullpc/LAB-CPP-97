9de77d033ca62d6dce63e85c7c4666e4c12a0bb1
Maaz Patwekar <242930825+nullpc@users.noreply.github.com>
2026-07-28 09:49:32 +0000

    commit

--- HOME WORK/Practical 1 /submulmodu.cpp
+++ HOME WORK/Practical 1 /submulmodu.cpp
@@ -9,7 +9,7 @@ int main() {
     int product = num1 * num2;
     std::cout << "Subtraction (Difference): " << difference << std::endl;
     std::cout << "Multiplication (Product): " << product << std::endl;
-    if (num2 != 0) 
+    if (num2 != 0)
     {
         int remainder = num1 % num2;
         std::cout << "Modulus (Remainder): " << remainder << std::endl;
