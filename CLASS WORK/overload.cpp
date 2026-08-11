0409ae78934b4307788e72b666c5d68ed626af83
Maaz Patwekar <242930825+nullpc@users.noreply.github.com>
2026-08-11 09:37:47 +0000

    commit

--- CLASS WORK/overload.cpp
+++ CLASS WORK/overload.cpp
@@ -1,24 +1,28 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int area (int);
-int area (int, int);
-float area (float);
+int area(int);
+int area(int, int);
+float area(float);
+
 int main()
 {
-    cout<< "Calling the area() function for computing the area of a square (side = 5) - " <<area(5)<<"\n";
-    cout<< "Calling the area() function for computing the area of a rectangle (length = 5, breadth = 10) - "<<area(5,10)<<"\n";
-    cout<< "Calling the area() function for computing the area of a circle (radius = 5.5) - " <<area(5.5);
+    cout << "Calling the area() function for computing the area of a square (side = 5) - " << area(5) << "\n";
+    cout << "Calling the area() function for computing the area of a rectangle (length = 5, breadth = 10) - " << area(5, 10) << "\n";
+    cout << "Calling the area() function for computing the area of a circle (radius = 5.5) - " << area(5.5f);
     return 0;
 }
-int area (int side)          
+
+int area(int side)          
 {
-    return(side*side);
+    return (side * side);
 }
-int area (int length, int breadth)
+
+int area(int length, int breadth)
 {
-    return(length*breadth);
+    return (length * breadth);
 }
-float area (float radius)       
+
+float area(float radius)       
 {
-    return(3.14*radius*radius);
+    return (3.14f * radius * radius);
 }
