9c80c928f5d157acab990fef0658e16409704cbc
Maaz Patwekar <242930825+nullpc@users.noreply.github.com>
2026-08-11 10:00:25 +0000

    commit

--- CLASS WORK/overload.cpp
+++ CLASS WORK/overload.cpp
@@ -9,6 +9,7 @@ int main()
     cout << "Calling the area() function for computing the area of a square (side = 5) - " << area(5) << "\n";
     cout << "Calling the area() function for computing the area of a rectangle (length = 5, breadth = 10) - " << area(5, 10) << "\n";
     cout << "Calling the area() function for computing the area of a circle (radius = 5.5) - " << area(5.5f);
+    cout <<"\n";
     return 0;
 }
 
